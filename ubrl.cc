150a012e3bff821fc8ae2289f7a17bf0fa5dd11f
Mario Lang <mlang@delysid.org>
2013-09-12 16:48:14 +0200

    Define BOOST_SPIRIT_NO_PREDEFINED_TERMINALS.

--- ubrl.cc
+++ ubrl.cc
@@ -1,4 +1,5 @@
 #include "ubrl.h"
+#define BOOST_SPIRIT_NO_PREDEFINED_TERMINALS
 #include <boost/spirit/include/qi_char_.hpp>
 #include <boost/spirit/include/qi_eol.hpp>
 #include <boost/spirit/include/qi_eoi.hpp>
@@ -15,7 +16,12 @@ ubrl::ubrl(Magick::Image &image)
   char const *begin = static_cast<char const *>(blob.data());
   char const *const end = begin + blob.length();
 
-  using namespace boost::spirit::qi;
+  boost::spirit::qi::eoi_type eoi;
+  boost::spirit::qi::eol_type eol;
+  boost::spirit::qi::lit_type lit;
+  boost::spirit::qi::uint_type uint_;
+  boost::spirit::qi::char_type char_;
+  boost::spirit::qi::
   parse( begin, end
        , lit("Width: ") > uint_ > eol
            > "Height: " > uint_ > eol
