801ce08a7068c6a95e9c1b04e1b57e12aa1a3fcd
Mario Lang <mlang@delysid.org>
2013-09-12 16:58:04 +0200

    Using namespace.

--- ubrl.cc
+++ ubrl.cc
@@ -1,4 +1,5 @@
 #include "ubrl.h"
+#include <Magick++/Blob.h>
 #define BOOST_SPIRIT_NO_PREDEFINED_TERMINALS
 #include <boost/spirit/include/qi_char_.hpp>
 #include <boost/spirit/include/qi_eol.hpp>
@@ -13,15 +14,16 @@ ubrl::ubrl(Magick::Image &image)
 {
   Magick::Blob blob;
   image.write(&blob, "ubrl");
+
+  using namespace boost::spirit::qi;
   char const *begin = static_cast<char const *>(blob.data());
   char const *const end = begin + blob.length();
+  char_type char_;
+  eoi_type eoi;
+  eol_type eol;
+  lit_type lit;
+  uint_type uint_;
 
-  boost::spirit::qi::eoi_type eoi;
-  boost::spirit::qi::eol_type eol;
-  boost::spirit::qi::lit_type lit;
-  boost::spirit::qi::uint_type uint_;
-  boost::spirit::qi::char_type char_;
-  boost::spirit::qi::
   parse( begin, end
        , lit("Width: ") > uint_ > eol
            > "Height: " > uint_ > eol
