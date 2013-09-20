92524c25821fb5bd7dd5517611c27d0ad58c0a78
Mario Lang <mlang@delysid.org>
2013-09-21 01:11:03 +0200

    Log Content-Language.

--- img2brl.cc
+++ img2brl.cc
@@ -377,7 +377,7 @@ int main()
   output_mode mode(output_mode::html);
   Cgicc cgi;
 
-  cerr << "Accept-Language: " << std::getenv("ACCEPT_LANGUAGE") << std::endl;
+  cerr << "Accept-Language: " << std::getenv("HTTP_ACCEPT_LANGUAGE") << std::endl;
   try {
     cerr << nounitbuf;
     if (cgi.getElement("mode") != cgi.getElements().end()) {
